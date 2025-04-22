a745e6bb58963b7590ef62c0c61fc3f48a49c73c
Bankole Theophilus <71837809+phynnoex@users.noreply.github.com>
2025-04-22 09:35:37 -0400

    fix: set basename for BrowserRouter in main.jsx

--- moviVideo/src/main.jsx
+++ moviVideo/src/main.jsx
@@ -8,7 +8,7 @@ import { AuthProvider } from "./Config/AuthContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename='/movieCatgeoryApp'>
         <App />
       </BrowserRouter>
     </AuthProvider>
